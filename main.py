e0c03258562275c27522d007d137832d60aa525e
lugerron <lugerron@burnedwoods.com>
2024-09-29 06:50:31 -0400

    Added asteroids, player, collisions and the game is starting to look like a game.

--- main.py
+++ main.py
@@ -3,17 +3,43 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
 	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen    = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock     = pygame.time.Clock()
+	updatable = pygame.sprite.Group()
+	drawable  = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	dt        = 0
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	player    = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		
+		for obj in updatable:
+			obj.update(dt)
+		
+		for obj in asteroids:
+			if obj.collision(player):
+				print("Game over!")
+				return
+
+		for obj in drawable:
+			obj.draw(screen)
+		
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
